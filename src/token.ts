58fa63b67108346b8344a5022f3ddc2b7ff5dc7e
Travis Harrison <travisharrisonyeah@gmail.com>
2024-10-14 20:33:25 -0500

    init tokens

---
+++ src/token.ts
@@ -0,0 +1,24 @@
+export class Token {
+  tokenType: TokenType;
+  lexeme: string;
+  literal: any;
+  line: number;
+
+  constructor(
+    tokenType: TokenType,
+    lexeme: string,
+    literal: any,
+    line: number,
+  ) {
+    this.tokenType = tokenType;
+    this.lexeme = lexeme;
+    this.literal = literal;
+    this.line = line;
+  }
+
+  toString(): string {
+    return `${this.tokenType} ${this.lexeme} ${this.literal}`;
+  }
+}
+
+export enum TokenType {}
