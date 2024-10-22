b52543c54527346e2bb9fab50bf6feeae6c3adae
Travis Harrison <travisharrisonyeah@gmail.com>
2024-10-21 23:52:35 -0500

    add some boilerplate ast

--- src/ast.ts
+++ src/ast.ts
@@ -6,6 +6,10 @@ export interface Expression {
 
 export interface ExpressionVisitor<T> {
   visitBooleanExpression(expr: BooleanExpression): T;
+  visitBinaryExpression(expr: BinaryExpression): T;
+  visitLogicalExpression(expr: LogicalExpression): T;
+  visitLiteralExpression(expr: LiteralExpression): T;
+  visitUnaryExpression(expr: UnaryExpression): T;
 }
 
 export class BooleanExpression implements Expression {
@@ -21,5 +25,63 @@ export class BooleanExpression implements Expression {
 
   accept<T>(visitor: ExpressionVisitor<T>): T {
     return visitor.visitBooleanExpression(this);
+  }
+}
+
+export class BinaryExpression implements Expression {
+  left: Expression;
+  operator: Token;
+  right: Expression;
+
+  constructor(left: Expression, operator: Token, right: Expression) {
+    this.left = left;
+    this.operator = operator;
+    this.right = right;
+  }
+
+  accept<T>(visitor: ExpressionVisitor<T>): T {
+    return visitor.visitBinaryExpression(this);
+  }
+}
+
+export class LogicalExpression implements Expression {
+  left: Expression;
+  operator: Token;
+  right: Expression;
+
+  constructor(left: Expression, operator: Token, right: Expression) {
+    this.left = left;
+    this.operator = operator;
+    this.right = right;
+  }
+
+  accept<T>(visitor: ExpressionVisitor<T>): T {
+    return visitor.visitLogicalExpression(this);
+  }
+}
+
+export class LiteralExpression implements Expression {
+  value: any;
+
+  constructor(value: any) {
+    this.value = value;
+  }
+
+  accept<T>(visitor: ExpressionVisitor<T>): T {
+    return visitor.visitLiteralExpression(this);
+  }
+}
+
+export class UnaryExpression implements Expression {
+  operator: Token;
+  right: Expression;
+
+  constructor(operator: Token, right: Expression) {
+    this.operator = operator;
+    this.right = right;
+  }
+
+  accept<T>(visitor: ExpressionVisitor<T>): T {
+    return visitor.visitUnaryExpression(this);
   }
 }
