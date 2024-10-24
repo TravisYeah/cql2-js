8f7efb3424b8d85bfbd4570d5c7d41402a01654b
Travis Harrison <travisharrisonyeah@gmail.com>
2024-10-24 07:04:03 -0500

    dedup scanner tests

--- tests/scanner.test.ts
+++ tests/scanner.test.ts
@@ -6,125 +6,103 @@ function logger(line: number, message: string) {
   console.log(line, message);
 }
 
+function scan(input: string, output: Token[]) {
+  const scanner = new Scanner(input, logger);
+  const tokens = scanner.scanTokens();
+  expect(tokens).toEqual(output);
+}
+
 describe("scanner", () => {
   test("EOF", async () => {
-    const scanner = new Scanner("", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([new Token(TokenType.EOF, "", null, 1)]);
+    scan("", [new Token(TokenType.EOF, "", null, 1)]);
   });
 
   test("LeftParen", async () => {
-    const scanner = new Scanner("(", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan("(", [
       new Token(TokenType.LeftParen, "(", null, 1),
       new Token(TokenType.EOF, "", null, 1),
     ]);
   });
 
   test("RightParen", async () => {
-    const scanner = new Scanner(")", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan(")", [
       new Token(TokenType.RightParen, ")", null, 1),
       new Token(TokenType.EOF, "", null, 1),
     ]);
   });
 
   test("Plus", async () => {
-    const scanner = new Scanner("+", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan("+", [
       new Token(TokenType.Plus, "+", null, 1),
       new Token(TokenType.EOF, "", null, 1),
     ]);
   });
 
   test("Minus", async () => {
-    const scanner = new Scanner("-", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan("-", [
       new Token(TokenType.Minus, "-", null, 1),
       new Token(TokenType.EOF, "", null, 1),
     ]);
   });
 
   test("Equal", async () => {
-    const scanner = new Scanner("=", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan("=", [
       new Token(TokenType.Equal, "=", null, 1),
       new Token(TokenType.EOF, "", null, 1),
     ]);
   });
 
   test("NotEqual", async () => {
-    const scanner = new Scanner("<>", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan("<>", [
       new Token(TokenType.NotEqual, "<>", null, 1),
       new Token(TokenType.EOF, "", null, 1),
     ]);
   });
 
   test("Greater", async () => {
-    const scanner = new Scanner(">", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan(">", [
       new Token(TokenType.Greater, ">", null, 1),
       new Token(TokenType.EOF, "", null, 1),
     ]);
   });
 
   test("GreaterEqual", async () => {
-    const scanner = new Scanner(">=", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan(">=", [
       new Token(TokenType.GreaterEqual, ">=", null, 1),
       new Token(TokenType.EOF, "", null, 1),
     ]);
   });
 
   test("Less", async () => {
-    const scanner = new Scanner("<", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan("<", [
       new Token(TokenType.Less, "<", null, 1),
       new Token(TokenType.EOF, "", null, 1),
     ]);
   });
 
   test("LessEqual", async () => {
-    const scanner = new Scanner("<=", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan("<=", [
       new Token(TokenType.LessEqual, "<=", null, 1),
       new Token(TokenType.EOF, "", null, 1),
     ]);
   });
 
   test("Comma", async () => {
-    const scanner = new Scanner(",", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan(",", [
       new Token(TokenType.Comma, ",", null, 1),
       new Token(TokenType.EOF, "", null, 1),
     ]);
   });
 
   test("String", async () => {
-    const scanner = new Scanner("'test'", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan("'test'", [
       new Token(TokenType.String, "'test'", "test", 1),
       new Token(TokenType.EOF, "", null, 1),
     ]);
   });
 
   test("String - empty", async () => {
-    const scanner = new Scanner("''", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan("''", [
       new Token(TokenType.String, "''", "", 1),
       new Token(TokenType.EOF, "", null, 1),
     ]);
@@ -138,36 +116,28 @@ describe("scanner", () => {
   });
 
   test("String - backslash escaped quote", async () => {
-    const scanner = new Scanner("'te\\'st'", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan("'te\\'st'", [
       new Token(TokenType.String, "'te\\'st'", "te'st", 1),
       new Token(TokenType.EOF, "", null, 1),
     ]);
   });
 
   test("String - two quote escaped quote", async () => {
-    const scanner = new Scanner("'te''st'", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan("'te''st'", [
       new Token(TokenType.String, "'te''st'", "te'st", 1),
       new Token(TokenType.EOF, "", null, 1),
     ]);
   });
 
   test("Numeric - 1", async () => {
-    const scanner = new Scanner("1", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan("1", [
       new Token(TokenType.Numeric, "1", 1, 1),
       new Token(TokenType.EOF, "", null, 1),
     ]);
   });
 
   test("Numeric - -1", async () => {
-    const scanner = new Scanner("-1", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan("-1", [
       new Token(TokenType.Minus, "-", null, 1),
       new Token(TokenType.Numeric, "1", 1, 1),
       new Token(TokenType.EOF, "", null, 1),
@@ -175,18 +145,14 @@ describe("scanner", () => {
   });
 
   test("Numeric - 1.2", async () => {
-    const scanner = new Scanner("1.2", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan("1.2", [
       new Token(TokenType.Numeric, "1.2", 1.2, 1),
       new Token(TokenType.EOF, "", null, 1),
     ]);
   });
 
   test("Numeric - -1.2", async () => {
-    const scanner = new Scanner("-1.2", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan("-1.2", [
       new Token(TokenType.Minus, "-", null, 1),
       new Token(TokenType.Numeric, "1.2", 1.2, 1),
       new Token(TokenType.EOF, "", null, 1),
@@ -216,18 +182,14 @@ describe("scanner", () => {
   });
 
   test("Identifier", async () => {
-    const scanner = new Scanner("test", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan("test", [
       new Token(TokenType.Identifier, "test", null, 1),
       new Token(TokenType.EOF, "", null, 1),
     ]);
   });
 
   test("Identifier - double quoted", async () => {
-    const scanner = new Scanner('"test"', logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan('"test"', [
       new Token(TokenType.DoubleQuote, '"', null, 1),
       new Token(TokenType.Identifier, "test", null, 1),
       new Token(TokenType.DoubleQuote, '"', null, 1),
@@ -236,99 +198,77 @@ describe("scanner", () => {
   });
 
   test("double quote", async () => {
-    const scanner = new Scanner('"', logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan('"', [
       new Token(TokenType.DoubleQuote, '"', null, 1),
       new Token(TokenType.EOF, "", null, 1),
     ]);
   });
 
   test("Star", async () => {
-    const scanner = new Scanner("*", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan("*", [
       new Token(TokenType.Star, "*", null, 1),
       new Token(TokenType.EOF, "", null, 1),
     ]);
   });
 
   test("Slash", async () => {
-    const scanner = new Scanner("/", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan("/", [
       new Token(TokenType.Slash, "/", null, 1),
       new Token(TokenType.EOF, "", null, 1),
     ]);
   });
 
   test("%", async () => {
-    const scanner = new Scanner("%", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan("%", [
       new Token(TokenType.Modulus, "%", null, 1),
       new Token(TokenType.EOF, "", null, 1),
     ]);
   });
 
   test("^", async () => {
-    const scanner = new Scanner("^", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan("^", [
       new Token(TokenType.Caret, "^", null, 1),
       new Token(TokenType.EOF, "", null, 1),
     ]);
   });
 
   test("TRUE", async () => {
-    const scanner = new Scanner("TRUE", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan("TRUE", [
       new Token(TokenType.True, "TRUE", null, 1),
       new Token(TokenType.EOF, "", null, 1),
     ]);
   });
 
   test("true", async () => {
-    const scanner = new Scanner("true", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan("true", [
       new Token(TokenType.True, "true", null, 1),
       new Token(TokenType.EOF, "", null, 1),
     ]);
   });
 
   test("FALSE", async () => {
-    const scanner = new Scanner("FALSE", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan("FALSE", [
       new Token(TokenType.False, "FALSE", null, 1),
       new Token(TokenType.EOF, "", null, 1),
     ]);
   });
 
   test("AND", async () => {
-    const scanner = new Scanner("AND", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan("AND", [
       new Token(TokenType.And, "AND", null, 1),
       new Token(TokenType.EOF, "", null, 1),
     ]);
   });
 
   test("OR", async () => {
-    const scanner = new Scanner("OR", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan("OR", [
       new Token(TokenType.Or, "OR", null, 1),
       new Token(TokenType.EOF, "", null, 1),
     ]);
   });
 
   test("NOT", async () => {
-    const scanner = new Scanner("NOT", logger);
-    const tokens = scanner.scanTokens();
-    expect(tokens).toEqual([
+    scan("NOT", [
       new Token(TokenType.Not, "NOT", null, 1),
       new Token(TokenType.EOF, "", null, 1),
     ]);
